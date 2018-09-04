b07d0b3861293dea7c13fcc4e5ad3279a6561288
Mirror198829 <412366756@qq.com>
2018-09-04 13:56:36 +0800

    修改代码

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -60,6 +60,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: config.build.index,
       template: 'index.html',
+      favicon: 'favicon.ico', 
       inject: true,
       minify: {
         removeComments: true,
