c0df7c1ca45739548ab413e57928294550e6c400
Mirror198829 <412366756@qq.com>
2018-09-04 11:49:59 +0800

    修改

--- config/index.js
+++ config/index.js
@@ -55,7 +55,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue2-topo-exhibition/',
+    assetsPublicPath: '/vue-topo/dist/',
+    //assetsPublicPath: '/',
 
     /**
      * Source Maps
